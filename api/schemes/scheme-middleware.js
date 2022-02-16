6d16f861d963351d97c11ef736152a455690378f
Brett Smith <brettsmith212@gmail.com>
2022-02-16 14:54:02 -0800

    find and findbyid done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const { add, addStep, find, findById, findSteps } = require("./scheme-model");
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +8,17 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  const { scheme_id } = req.params;
+  console.log(scheme_id);
+  const scheme = await findById(scheme_id);
+  if (!scheme) {
+    res.status(404).json({ message: `scheme with scheme_id ${id} not found` });
+    return;
+  }
 
-}
+  next();
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +28,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +39,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,13 @@
-function find() { // EXERCISE A
+const db = require("../../data/db-config");
+
+function find() {
+  // EXERCISE A
+  return db("schemes as sc")
+    .leftJoin("steps as st", "st.scheme_id", "sc.scheme_id")
+    .groupBy("sc.scheme_id")
+    .select("sc.*")
+    .count("st.step_id as number_of_steps")
+    .orderBy("sc.scheme_id", "ASC");
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
@@ -17,7 +26,37 @@ function find() { // EXERCISE A
   */
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) {
+  const result = await db("schemes as sc")
+    .leftJoin("steps as st", "st.scheme_id", "sc.scheme_id")
+    .select("sc.scheme_name", "st.*")
+    .where({ "sc.scheme_id": scheme_id })
+    .orderBy("st.step_number", "ASC");
+
+  if (result.length === 0) {
+    return null;
+  }
+
+  const scheme = {
+    scheme_id: result[0].scheme_id,
+    scheme_name: result[0].scheme_name,
+    steps: [],
+  };
+
+  if (result[0].step_id === null) {
+    return scheme;
+  }
+
+  for (let step of result) {
+    scheme.steps.push({
+      step_id: step.step_id,
+      step_number: step.step_number,
+      instructions: step.instructions,
+    });
+  }
+
+  return scheme;
+  // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -85,7 +124,15 @@ function findById(scheme_id) { // EXERCISE B
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) {
+  const result = await db("schemes as sc")
+    .leftJoin("steps as st", "sc.scheme_id", "st.scheme_id")
+    .select("st.step_id", "st.step_number", "st.instructions", "sc.scheme_name")
+    .where({ "sc.scheme_id": scheme_id })
+    .orderBy("st.step_number", "ASC");
+
+  return result;
+  // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -108,13 +155,15 @@ function findSteps(scheme_id) { // EXERCISE C
   */
 }
 
-function add(scheme) { // EXERCISE D
+function add(scheme) {
+  // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+function addStep(scheme_id, step) {
+  // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
@@ -128,4 +177,4 @@ module.exports = {
   findSteps,
   add,
   addStep,
-}
+};
